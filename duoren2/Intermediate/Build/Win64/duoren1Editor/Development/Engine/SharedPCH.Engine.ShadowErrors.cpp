d5bfac1d2535b5cc0041f5dba5c243b6c3519b0e
nietzstl <1054893827@qq.com>
2021-06-14 22:16:17 +0800

    Revert "first"
    This reverts commit e0669824963ca1f432fb68952d4967baaf2a22fe.

--- duoren2/Intermediate/Build/Win64/duoren1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29336
-#include "D:/demos/duoren2/Intermediate/Build/Win64/duoren1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- duoren2/Source/duoren1/duoren1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "duoren1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, duoren1, "duoren1" );
- 
